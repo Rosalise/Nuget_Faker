c16602d02c23c7ffb5637fa18b6d87a05676f4ab
Rosalise <kapsophia@gmail.com>
2022-10-05 14:34:43 +0300

    Add NugetExample

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var name = Faker.Name.FullName();
+        var city = Faker.Address.City();
+        var address = Faker.Address.StreetAddress();
+        
+        var random = new Random();
+        var letter = Convert.ToChar(random.Next('A', 'Z')); 
+
+       var tunnus = Faker.RandomNumber.Next(0, 100000000);
+    
+        
+        Console.WriteLine($"Name: {name}. Osoite: {city} {address}. Henkilötunnus: {letter} {tunnus}");
+    }
+}
